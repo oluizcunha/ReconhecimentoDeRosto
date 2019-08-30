3bada72ddc74d2f03ba69a1fdefaff601c1f9247
Luiz Cunha <luizotcunha@gmail.com>
2019-08-30 11:44:37 -0400

    inserindo comentarios em portugues

--- facial_landmarks.py
+++ facial_landmarks.py
@@ -9,7 +9,7 @@ import imutils
 import dlib
 import cv2
 
-# construct the argument parser and parse the arguments
+# constrói o analisador de argumentos e analisa os argumentos
 ap = argparse.ArgumentParser()
 ap.add_argument("-p", "--shape-predictor", required=True,
 	help="path to facial landmark predictor")
@@ -17,41 +17,47 @@ ap.add_argument("-i", "--image", required=True,
 	help="path to input image")
 args = vars(ap.parse_args())
 
-# initialize dlib's face detector (HOG-based) and then create
-# the facial landmark predictor
+
+# inicialize o detector de rosto do dlib (baseado em HOG) e crie
+# o indicador de marco facial
+
 detector = dlib.get_frontal_face_detector()
 predictor = dlib.shape_predictor(args["shape_predictor"])
 
-# load the input image, resize it, and convert it to grayscale
+# carrega a imagem de entrada, redimensione-a e converta-a em escala de cinza
 image = cv2.imread(args["image"])
 image = imutils.resize(image, width=500)
 gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
 
-# detect faces in the grayscale image
+
+# detectar rostos na imagem em escala de cinza
 rects = detector(gray, 1)
 
-# loop over the face detections
+
+# laço nas detecções de rosto
 for (i, rect) in enumerate(rects):
-	# determine the facial landmarks for the face region, then
-	# convert the facial landmark (x, y)-coordinates to a NumPy
-	# array
+# determine os pontos de referência faciais da região do rosto e, em seguida,
+# converte o marco facial (x, y) -coordena em um NumPy
+# array
 	shape = predictor(gray, rect)
 	shape = face_utils.shape_to_np(shape)
 
-	# convert dlib's rectangle to a OpenCV-style bounding box
-	# [i.e., (x, y, w, h)], then draw the face bounding box
+
+# converter o retângulo do dlib em uma caixa delimitadora no estilo OpenCV
+# [ou seja, (x, y, w, h)] e desenhe a caixa delimitadora da face
 	(x, y, w, h) = face_utils.rect_to_bb(rect)
 	cv2.rectangle(image, (x, y), (x + w, y + h), (0, 255, 0), 2)
 
-	# show the face number
+
+# mostra o número do rosto
 	cv2.putText(image, "Face #{}".format(i + 1), (x - 10, y - 10),
 		cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 0), 2)
 
-	# loop over the (x, y)-coordinates for the facial landmarks
-	# and draw them on the image
+# loop sobre as coordenadas (x, y) para os pontos de referência faciais
+# e desenhe-os na imagem
 	for (x, y) in shape:
 		cv2.circle(image, (x, y), 1, (0, 0, 255), -1)
 
-# show the output image with the face detections + facial landmarks
+# mostra a imagem de saída com as detecções de rosto + pontos faciais
 cv2.imshow("Output", image)
 cv2.waitKey(0)
